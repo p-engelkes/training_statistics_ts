e83250f670b63d4fa9a33464514a723f9a9fa871
pengelkes <patrick.engelkes@gmail.com>
2017-09-05 17:50:23 +0200

    Add Material-Ui.

--- src/app.tsx
+++ src/app.tsx
@@ -2,11 +2,14 @@ import configureStore from './store';
 import * as React from "react";
 import {Provider} from "react-redux";
 import {Home} from "./home";
+import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
 
 const store = configureStore();
 
 export const App = () => (
     <Provider store={store}>
-        <Home />
+        <MuiThemeProvider>
+            <Home />
+        </MuiThemeProvider>
     </Provider>
 );
